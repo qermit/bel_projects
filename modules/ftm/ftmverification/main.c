29b383b9e6e0be458d1f06cd7b7bad3334d2b7c9
Mathias Kreider <m.kreider@gsi.de>
2014-11-24 15:20:18 +0100

    FTM:  bugfix for verification fw output

--- modules/ftm/ftmverification/main.c
+++ modules/ftm/ftmverification/main.c
@@ -34,12 +34,12 @@ static char* u64toStr(char* s, uint64_t val, uint8_t len)
    const unsigned long long divStart = 10000000000000000000ULL;
    memset(s, 0x0, len);
    char*    p     = s;
+   char*    end   = p + (uintptr_t)len -1;
    uint64_t tmp   = val;
    uint64_t div   = (uint64_t)divStart;
-   uint8_t  idx;
    
-   for (idx = 0; idx<(len-1); idx++) {
-      p[idx]   = (char)(tmp / div + '0');
+   while(p <= end && div) {
+      *p++   = (char)(tmp / div + '0');
       tmp     %= div;
       div     /= 10;
    }
@@ -54,17 +54,62 @@ static char* i64toStr(char* s, int64_t val, uint8_t len)
    
    if (val < 0) {
        *p++ = '-';
-       len--;
-   }    
-   u64toStr(p, (uint64_t)val, 21);     
+       val = -val;
+   } else *p++ = ' ';    
+   u64toStr(p, (uint64_t)val, len);     
+   return s;
+}
+
+static char* milPt(char* out, int64_t val)
+{
+   uint8_t len = 21;
+   memset(out, ' ', len+3);
+   char buf[len]; 
+   char* s = &buf[0];
+   i64toStr(s, val, len);
+   char*  end = s + (uintptr_t)len -1;
+   char*  p   = end;
+   char* po   = out + (uintptr_t)len + 3 -1;
+
+  
+   while(p >= end-9) {
+      if ((end - p) && !((end - p) % 3)) {
+      // insert point
+         *po-- = '.';
+         *po-- = *p--;
+      }
+      else  *po-- = *p--; //copy 
+   }
+   
+   while(p >= s) {
+     *po-- = *p--;   
+   }
+   
+     
+   return out;
+}
+
+static char* remLz(char* s)
+{
+   char*  p   = s;
+   char*  e   = p+1;
+   uint8_t lz = 1;
+  
+   while(*e) {
+      if (lz) {
+         if((*p == '.') || (*p == '0') || (*p == ' ')) *p++ = ' ';
+         else lz = 0; 
+      }
+      else  p++;
+      e   = p+1;
+   }
    return s;
 }
 
 
-
 void report(uint64_t now, uint32_t actCnt, uint32_t errCnt)
 {
-   const uint32_t bufSize = 21;
+   const uint32_t bufSize = 24;
    char buf[bufSize];
    
    //acquire info on offending evt
@@ -72,7 +117,7 @@ void report(uint64_t now, uint32_t actCnt, uint32_t errCnt)
    uint64_t par   = ((uint64_t)*(pEC + (ECA_EVT_PA_HI >> 2))) << 32 | (uint64_t)*(pEC + (ECA_EVT_PA_LO >> 2));
    uint64_t time  = (((uint64_t)*(pEC + (ECA_EVT_TI_HI >> 2))) << 32 | (uint64_t)*(pEC + (ECA_EVT_TI_LO >> 2))) << 3;
     
-   mprintf("################## %x/%x ##################", errCnt, actCnt);
+   mprintf("################## %u/%u ##################", errCnt, actCnt);
    if( *(pEC + (ECA_FLAGS  >> 2)) & 0x01 ) mprintf("late ");
    if( *(pEC + (ECA_FLAGS  >> 2)) & 0x02 ) mprintf("conflict ");
    mprintf("\n");
@@ -81,24 +126,36 @@ void report(uint64_t now, uint32_t actCnt, uint32_t errCnt)
    
    
    mprintf("par:\t0x%08x%08x\n", (uint32_t)(par>>32), (uint32_t)par);
+   mprintf("tef:\t\t0x%08x\ntag:\t\t0x%08x\n", *(pEC + (ECA_EVT_TEF >> 2)), *(pEC + (ECA_EVT_TAG >> 2)) );
+   mprintf("                   s   ms  us  ns\n");
+
+   
+   remLz(milPt(buf, now));
+   //u64toStr(buf, now, bufSize);
+   mprintf("now:     %s\n", buf);  
+   //u64toStr(buf, time, bufSize);
+   remLz(milPt(buf, time));
+   mprintf("ts:      %s\n",  buf);
    // does par start with 0xff? if so, then 
    if((par >> 56) == 0xff) {
+      uint64_t tsdbg = ((par & (~(0xffULL<<56)))<<3);
       // this is a debug timestamp
       // strip the leading 0xff and convert to decimal
-      u64toStr(buf, par & (~(0xffULL<<56)), bufSize);
-      mprintf("tsdgb:\t%s\n",  buf);   
+      //u64toStr(buf, tsdbg, bufSize);
+      remLz(milPt(buf, tsdbg));
+      mprintf("tsdsp:   %s\n",  buf);
+      //i64toStr(buf, ((int64_t)(now) - (int64_t)(tsdbg)), bufSize);
+      remLz(milPt(buf, ((int64_t)(now) - (int64_t)(tsdbg))));
+      mprintf("now-dsp: %s\n", buf);
+      remLz(milPt(buf, ((int64_t)(time) - (int64_t)(tsdbg))));
+      mprintf("ts-dsp:  %s\n", buf);    
    }
-   mprintf("tef:\t\t0x%08x\ntag:\t\t0x%08x\n", 
-    *(pEC + (ECA_EVT_TEF >> 2)), *(pEC + (ECA_EVT_TAG >> 2)) );
+  
    
-   u64toStr(buf, time, bufSize);
-   mprintf("ts:\t%s\n",  buf);
    
-   u64toStr(buf, now, bufSize);
-   mprintf("now:\t%s\n", buf, (uint32_t)(now>>32), (uint32_t)now);
-   
-   i64toStr(buf, ((int64_t)(now) - (int64_t)(time)), bufSize);
-   mprintf("dif:\t%s\n", buf); 
+   remLz(milPt(buf, ((int64_t)(now) - (int64_t)(time))));
+   //i64toStr(buf, ((int64_t)(now) - (int64_t)(time)), bufSize);
+   mprintf("now-ts:  %s\n", buf); 
    
  }
 
