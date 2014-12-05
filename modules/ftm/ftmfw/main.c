a6d43260d7f4a35263e335dcdea8f8b4e7374d64
Mathias Kreider <m.kreider@gsi.de>
2014-12-05 11:53:05 +0100

    ftmfw: added warning messages on startup re debug mode, better atomic output routine

--- modules/ftm/ftmfw/main.c
+++ modules/ftm/ftmfw/main.c
@@ -44,9 +44,6 @@ void ebmInit()
 
 void init()
 { 
-   char buffer[] = "\nFTM Core #00 rdy ________"; 
-   char itoaBuffer[3];
-   
    discoverPeriphery();
    uart_init_hw();
    ebmInit();
@@ -54,21 +51,13 @@ void init()
  
    cmdCnt = 0;
    cpuId = getCpuIdx();
-   itoa(cpuId, itoaBuffer, 10);
-   itoa((uint32_t)&pFtmIf->cmd, &buffer[18], 8);
-   if(itoaBuffer[1]) {buffer[12] = itoaBuffer[1]; buffer[11] = itoaBuffer[0];}
-   if(itoaBuffer[0]) {buffer[12] = itoaBuffer[0];}
-
+   
    isr_table_clr();
    isr_ptr_table[0] = isr0; //timer
    isr_ptr_table[1] = isr1;   
    irq_set_mask(0x03);
    irq_enable();
    
-   
-   atomic_on(); 
-   uart_write_string(buffer);
-   atomic_off(); 
 }
 
 
@@ -122,9 +111,28 @@ void main(void) {
    init();
 
    for (j = 0; j < (125000000/4); ++j) { asm("nop"); }
-   mprintf("\n");
-
+   atomic_on(); 
+   mprintf("#%02u: Core ready\n", cpuId);
+   mprintf("#%02u: Shared @ 0x%08x.\n", cpuId, (uint32_t)pFtmIf->pSharedMem);
+   #if DEBUGLEVEL != 0
+      mprintf("#%02u: Debuglevel %u. Don't expect timeley delivery with console outputs on!\n", cpuId, DEBUGLEVEL);
+   #endif   
+   #if DEBUGTIME == 1
+      mprintf("#%02u: Debugtime mode ON. Par Field of Msgs will be overwritten be dispatch time at lm32\n", cpuId);
+   #endif
+   atomic_off();
+   *((uint32_t*)pFtmIf->pSharedMem + cpuId) = 0xDEAD0000 | cpuId;
    //hexDump ("Plan 0 Chain 0 : \n", (void*)pFtmIf->pAct->plans[0], 128);
+   /*
+   t_time now, later;
+   int64_t diff, diffSum;
+   int64_t diffMin, diffMax, diffAvg;
+   int64_t div = 1000000;
+   diffMin =  0x7fffffff;
+   diffMax =  0xffffffff;
+   diffAvg =  0;
+   */
+   
    while (1) {
       cmdEval();
       processFtm();
@@ -139,6 +147,18 @@ void main(void) {
       ebm_op(0x0, 0x1BEEBABE, EBM_WRITE);
       atomic_off();
       ebm_flush();
+      
+      for (j = 0; j < div; ++j) {
+         now   = getSysTime();
+         later = getSysTime();
+         diff  = (int64_t) later - (int64_t) now;
+         //mprintf("Min: %d Max: %d\n", (int32_t)diffMin, (int32_t)diffMax);
+         if(diffMin > diff) diffMin = diff;
+         if(diffMax < diff) diffMax = diff;
+         diffSum += diff;
+      }
+      mprintf("Min: %d Max: %d Avg: %d\n", (int32_t)diffMin, (int32_t)diffMax, ((int32_t)(diffSum / div)));
+      diffSum = 0;
       */
    }
 
