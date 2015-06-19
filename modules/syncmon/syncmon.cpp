4f7439d40dd56444a0157e6b0185bd91f592ec8d
A. Hahn <a.hahn@gsi.de>
2015-06-19 10:43:16 +0200

    syncmom: improved version, statistics, debug dump, ...

--- modules/syncmon/syncmon.cpp
+++ modules/syncmon/syncmon.cpp
@@ -45,20 +45,18 @@ using namespace GSI_TLU;
 
 /* Defines */
 /* ==================================================================================================== */
-#define EXPLODER5_IOS 16
-#define EXPLODER5_LEMO_OFFSET 4
-#define LEMO_OE_OFFSET_REG 4
-#define EVENTS 2000
-#define MAX_JITTER_NS 2
-#define EXPECTED_PULSE_DIFF_NS   100000000
-#define MAX_ALLOWED_JITTER_NS 200
+#define EXPLODER5_IOS             16
+#define EXPLODER5_LEMO_OFFSET     4
+#define EXPLODER5_OE_SETUP        0x0000
+#define LEMO_OE_OFFSET_REG        4
+#define MAX_JITTER_NS             2
+#define MAX_ALLOWED_JITTER_NS     200
+#define EXPECTED_PULSE_DIFF_NS    100000000
+#define TLU_ID                    0x4d78adfdU
+#define TLU_VENDOR                0x651
 
-volatile sig_atomic_t flag = 0;
-void vSignalHandler(int sig)
-{
-  flag = 1;
-}
-
+/* Structures */
+/* ==================================================================================================== */
 typedef struct
 {
   uint64_t uTotalEvents;
@@ -67,10 +65,23 @@ typedef struct
   uint64_t uMinPast;
   uint64_t uMaxFuture;
   uint64_t uMinFuture;
-  uint64_t uAverage;
-  uint64_t uStdDeviation;
+  double uAverage;
+  double uStdDeviation;
 } s_IOMeasurement;
 
+/* Global */
+/* ==================================================================================================== */
+volatile sig_atomic_t flag = 0;
+
+/* Function vSignalHandler */
+/* ==================================================================================================== */
+void vSignalHandler(int sig)
+{
+  flag = 1;
+}
+
+/* Function main */
+/* ==================================================================================================== */
 int main (int argc, const char** argv)
 {
 
@@ -80,35 +91,29 @@ int main (int argc, const char** argv)
   status_t status;
   std::vector<TLU> tlus;
   std::vector<std::vector<uint64_t> > queues;
-
   s_IOMeasurement a_sIOMeasurement[EXPLODER5_IOS];
-  int test = 0;
-
-
-  //uint32_t a_uEdges[EVENTS];
-  //uint64_t a_uFrequency[EVENTS];
-  
   Table table;
-  uint32_t uQueueInterator = 0;
+  uint32_t uQueueIterator = 0;
   uint32_t uQueueItemIterator = 0;
   uint32_t uQueuesTotal = 0;
   uint32_t uQueneItems = 0;
   uint64_t uTimeDiff = 0;
+  uint32_t uIterator = 0;
 
-
-  for(test = 0; test < 16; test++)
+  /* Clean results */
+  for(uIterator = 0; uIterator < EXPLODER5_IOS; uIterator++)
   {
-    a_sIOMeasurement[test].uTotalEvents = 0;
-    a_sIOMeasurement[test].uLastTimestamp = 0;
-    a_sIOMeasurement[test].uMaxPast = 0;
-    a_sIOMeasurement[test].uMinPast = 0;
-    a_sIOMeasurement[test].uMaxFuture = 0;
-    a_sIOMeasurement[test].uMinFuture = 0;
-    a_sIOMeasurement[test].uAverage = 0;
-    a_sIOMeasurement[test].uStdDeviation = 0;
+    a_sIOMeasurement[uIterator].uTotalEvents = 0;
+    a_sIOMeasurement[uIterator].uLastTimestamp = 0;
+    a_sIOMeasurement[uIterator].uMaxPast = 0;
+    a_sIOMeasurement[uIterator].uMinPast = 0;
+    a_sIOMeasurement[uIterator].uMaxFuture = 0;
+    a_sIOMeasurement[uIterator].uMinFuture = 0;
+    a_sIOMeasurement[uIterator].uAverage = 0;
+    a_sIOMeasurement[uIterator].uStdDeviation = 0;
   }
 
-
+  /* Setup signal handler */
   signal(SIGINT, vSignalHandler); 
 
   /* Try to open a (etherbone-) socket */
@@ -136,63 +141,86 @@ int main (int argc, const char** argv)
   
   /* Find the IO reconfig to enable/disable outputs to specific IOs */
   std::vector<sdb_device> devs;
-  device.sdb_find_by_identity(0x651, 0x4d78adfdU, devs);
+  device.sdb_find_by_identity(TLU_VENDOR, TLU_ID, devs);
   assert (devs.size() == 1);
   address_t ioconf = devs[0].sdb_component.addr_first + LEMO_OE_OFFSET_REG;
-  device.write(ioconf, EB_DATA32, 0x00);
+  device.write(ioconf, EB_DATA32, EXPLODER5_OE_SETUP);
   
   /* Check TLU */
-  while (test!=1)
+  while (true)
   {
 
     if(flag)
     {
-      printf("\nSignal caught!\n");
-      printf("default action is termination!\n");
+      /* Create debug dump */
+      for(uIterator = 0; uIterator < EXPLODER5_IOS; uIterator++)
+      {
+        /* Did we capture any event here? */
+        if(a_sIOMeasurement[uIterator].uTotalEvents)
+        {
+          fprintf(stdout, "%s: Results for IO %d:\n", argv[0], uIterator);
+          fprintf(stdout, "%s:   Events:           %ld\n", argv[0], a_sIOMeasurement[uIterator].uTotalEvents);
+          fprintf(stdout, "%s:   Latest Timestamp: %ld\n", argv[0], a_sIOMeasurement[uIterator].uLastTimestamp);
+          fprintf(stdout, "%s:   Max. Past:        %ldns\n", argv[0], a_sIOMeasurement[uIterator].uMaxPast);
+          fprintf(stdout, "%s:   Min. Past:        %ldns\n", argv[0], a_sIOMeasurement[uIterator].uMinPast);
+          fprintf(stdout, "%s:   Max. Future:      %ldns\n", argv[0], a_sIOMeasurement[uIterator].uMaxFuture);
+          fprintf(stdout, "%s:   Min. Future:      %ldns\n", argv[0], a_sIOMeasurement[uIterator].uMinFuture);
+          /* Calculate statistics */
+          a_sIOMeasurement[uIterator].uAverage /= a_sIOMeasurement[uIterator].uTotalEvents;
+          fprintf(stdout, "%s:   Average:          %fns\n", argv[0], a_sIOMeasurement[uIterator].uAverage);
+          
+        }
+      }
       flag = 0;
       exit(0);
     }
-    
+
     /* TODO: Poll device */
     usleep(500000);
 
     /* Read-out result */
     tlu.pop_all(queues);
     uQueuesTotal = queues.size();
-    
-    fprintf(stdout, "\n\n%s: Latest TS                            Count          Offset ts0          MaxFuture  MinFuture  MaxPast  MinPast\n", argv[0]);
-    fprintf(stdout, "%s: ------------------------------------------------------------------------------------------------------------------\n", argv[0]);
 
     /* Check each queue now */
-    for(uQueueInterator=0; uQueueInterator<uQueuesTotal; uQueueInterator++)
+    for(uQueueIterator=0; uQueueIterator<uQueuesTotal; uQueueIterator++)
     {
       
-       
-      std::vector<uint64_t>& queue = queues[uQueueInterator];
+      std::vector<uint64_t>& queue = queues[uQueueIterator];
       uQueneItems = queue.size(); /* Get the actual size */
-       
-       
-      if(uQueneItems!=0)
-      {
-        //fprintf(stdout, "%s: queue %d has a size of %d ...\n", argv[0], uQueueInterator, uQueneItems);
-      }
+      
       /* Inspect items with queue contains data */
-      if(uQueneItems)
+      if(uQueneItems > a_sIOMeasurement[EXPLODER5_LEMO_OFFSET].uTotalEvents)
       {
-        
-          // TODO: Type fix needed: interator
-          a_sIOMeasurement[uQueueInterator].uTotalEvents = uQueneItems;
-          a_sIOMeasurement[uQueueInterator].uLastTimestamp = queue[uQueneItems-1];
+        system("clear");
+        fprintf(stdout, "%s: Latest TS               Count          Offset ts0          MaxFuture  MinFuture  MaxPast  MinPast\n", argv[0]);
+        fprintf(stdout, "%s: ------------------------------------------------------------------------------------------------------------------\n", argv[0]);
+      }
+      
+      if(uQueneItems > a_sIOMeasurement[uQueueIterator].uTotalEvents)
+      {
           
+          /* Calculate time difference */
           uTimeDiff = queue[uQueneItems-1]-a_sIOMeasurement[EXPLODER5_LEMO_OFFSET].uLastTimestamp;
+        
+          /* Event seen */
+          if(uQueneItems > a_sIOMeasurement[uQueueIterator].uTotalEvents)
+          {
+            a_sIOMeasurement[uQueueIterator].uAverage += (int64_t)uTimeDiff;
+            //fprintf(stdout, "%s: %f\n", argv[0], a_sIOMeasurement[uQueueIterator].uAverage);
+          }
+        
+          a_sIOMeasurement[uQueueIterator].uTotalEvents = uQueneItems;
+          a_sIOMeasurement[uQueueIterator].uLastTimestamp = queue[uQueneItems-1];
+          
           
           /* Ignore broken LEMO */
-          if(!(uQueueInterator-EXPLODER5_LEMO_OFFSET == 5))
+          if(!(uQueueIterator-EXPLODER5_LEMO_OFFSET == 5))
           {
-            fprintf(stdout, "%s: ts%d: %ld                      %d", argv[0], uQueueInterator-EXPLODER5_LEMO_OFFSET, queue[uQueneItems-1], uQueneItems);
+            fprintf(stdout, "%s: ts%d: %ld       %d", argv[0], uQueueIterator-EXPLODER5_LEMO_OFFSET, queue[uQueneItems-1], uQueneItems);
           }
           
-          if (uQueueInterator-EXPLODER5_LEMO_OFFSET == 0)
+          if (uQueueIterator-EXPLODER5_LEMO_OFFSET == 0)
           {
             fprintf(stdout, "\n");
           }
@@ -202,47 +230,41 @@ int main (int argc, const char** argv)
             if ((int64_t)uTimeDiff < 0)
             {
               /* Update differences */
-              if((int64_t)uTimeDiff<(int64_t)a_sIOMeasurement[uQueueInterator].uMaxFuture) { a_sIOMeasurement[uQueueInterator].uMaxFuture = uTimeDiff; }
-              if((int64_t)uTimeDiff>(int64_t)a_sIOMeasurement[uQueueInterator].uMinFuture) { a_sIOMeasurement[uQueueInterator].uMinFuture = uTimeDiff; }
+              if((int64_t)uTimeDiff<(int64_t)a_sIOMeasurement[uQueueIterator].uMaxFuture) { a_sIOMeasurement[uQueueIterator].uMaxFuture = uTimeDiff; }
+              if((int64_t)uTimeDiff>(int64_t)a_sIOMeasurement[uQueueIterator].uMinFuture) { a_sIOMeasurement[uQueueIterator].uMinFuture = uTimeDiff; }
               /* This is the first time we see an event, set time marks */
               if(uQueneItems==1)
               {
-                a_sIOMeasurement[uQueueInterator].uMaxFuture = uTimeDiff;
-                a_sIOMeasurement[uQueueInterator].uMinFuture = uTimeDiff;
+                a_sIOMeasurement[uQueueIterator].uMaxFuture = uTimeDiff;
+                a_sIOMeasurement[uQueueIterator].uMinFuture = uTimeDiff;
               }
             }
             else if ((int64_t)uTimeDiff > 0)
             {
               /* Update differences */
-              if((int64_t)uTimeDiff>(int64_t)a_sIOMeasurement[uQueueInterator].uMaxPast) { a_sIOMeasurement[uQueueInterator].uMaxPast = uTimeDiff; }
-              if((int64_t)uTimeDiff<(int64_t)a_sIOMeasurement[uQueueInterator].uMinPast) { a_sIOMeasurement[uQueueInterator].uMinPast = uTimeDiff; }
+              if((int64_t)uTimeDiff>(int64_t)a_sIOMeasurement[uQueueIterator].uMaxPast) { a_sIOMeasurement[uQueueIterator].uMaxPast = uTimeDiff; }
+              if((int64_t)uTimeDiff<(int64_t)a_sIOMeasurement[uQueueIterator].uMinPast) { a_sIOMeasurement[uQueueIterator].uMinPast = uTimeDiff; }
               /* This is the first time we see an event, set time marks */
               if(uQueneItems==1)
               {
-                a_sIOMeasurement[uQueueInterator].uMaxPast = uTimeDiff;
-                a_sIOMeasurement[uQueueInterator].uMinPast = uTimeDiff;
+                a_sIOMeasurement[uQueueIterator].uMaxPast = uTimeDiff;
+                a_sIOMeasurement[uQueueIterator].uMinPast = uTimeDiff;
               }
             }
             else
             {
               /* Set minimal values to zero, because there is no difference */
-              a_sIOMeasurement[uQueueInterator].uMinFuture = 0;
-              a_sIOMeasurement[uQueueInterator].uMinPast = 0;
+              a_sIOMeasurement[uQueueIterator].uMinFuture = 0;
+              a_sIOMeasurement[uQueueIterator].uMinPast = 0;
             }
             
+            fprintf(stdout, "          %ldns               %ldns     %ldns     %ldns     %ldns\n", uTimeDiff, 
+                                                                              a_sIOMeasurement[uQueueIterator].uMaxFuture,
+                                                                              a_sIOMeasurement[uQueueIterator].uMinFuture,
+                                                                              a_sIOMeasurement[uQueueIterator].uMaxPast,
+                                                                              a_sIOMeasurement[uQueueIterator].uMinPast);
+                                                                              
 
-
-
-            /* TODO: REMOVE THIS LATER */
-            /* Ignore broken LEMO */
-            if(!(uQueueInterator-EXPLODER5_LEMO_OFFSET == 5))
-            {
-              fprintf(stdout, "          %ldns               %ldns     %ldns     %ldns     %ldns\n", uTimeDiff, 
-                                                                                a_sIOMeasurement[uQueueInterator].uMaxFuture,
-                                                                                a_sIOMeasurement[uQueueInterator].uMinFuture,
-                                                                                a_sIOMeasurement[uQueueInterator].uMaxPast,
-                                                                                a_sIOMeasurement[uQueueInterator].uMinPast);
-            }
             
           }
         
@@ -250,8 +272,7 @@ int main (int argc, const char** argv)
     }
   }
 
-  fprintf(stdout, "%s: done!\n", argv[0]);
-  
+  /* Should never get here */
   return 0;
   
 }
