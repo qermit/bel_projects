d64f1dc93289c3a32be10365888f480e7921a068
Mathias Kreider <mkreider@gsi.de>
2015-12-04 17:06:15 +0100

    ftm-ctl: updated to work with prioq2

--- modules/ftm/ftmx86/hwregs.h
+++ modules/ftm/ftmx86/hwregs.h
@@ -1,3 +1,8 @@
+#ifndef _HW_REGS_
+#define _HW_REGS_
+
+#include "prio_regs.h"
+
 #define EBM_REG_CLEAR         0                         
 #define EBM_REG_FLUSH         (EBM_REG_CLEAR        +4)        
 #define EBM_REG_STATUS        (EBM_REG_FLUSH        +4)         
@@ -21,66 +26,4 @@
 #define PPS_VALID      0x2
 #define TS_VALID      0x4
 
-// Priority Queue RegisterLayout
-static const struct {
-   uint32_t rst;
-   uint32_t force;
-   uint32_t dbgSet;
-   uint32_t dbgGet;
-   uint32_t clear;
-   uint32_t cfgGet;
-   uint32_t cfgSet;
-   uint32_t cfgClr;
-   uint32_t dstAdr;
-   uint32_t heapCnt;
-   uint32_t msgCntO;
-   uint32_t msgCntI;
-   uint32_t tTrnHi;
-   uint32_t tTrnLo;
-   uint32_t tDueHi;
-   uint32_t tDueLo;
-   uint32_t capacity;
-   uint32_t msgMax;
-   uint32_t ebmAdr;
-   uint32_t tsAdr;
-   uint32_t tsCh;
-   uint32_t cfg_ENA;
-   uint32_t cfg_FIFO;    
-   uint32_t cfg_IRQ;
-   uint32_t cfg_AUTOPOP;
-   uint32_t cfg_AUTOFLUSH_TIME;
-   uint32_t cfg_AUTOFLUSH_MSGS;
-   uint32_t cfg_MSG_ARR_TS;
-   uint32_t force_POP;
-   uint32_t force_FLUSH;
-} r_FPQ = {    .rst        =  0x00,
-               .force      =  0x04,
-               .dbgSet     =  0x08,
-               .dbgGet     =  0x0c,
-               .clear      =  0x10,
-               .cfgGet     =  0x14,
-               .cfgSet     =  0x18,
-               .cfgClr     =  0x1C,
-               .dstAdr     =  0x20,
-               .heapCnt    =  0x24,
-               .msgCntO    =  0x28,
-               .msgCntI    =  0x2C,
-               .tTrnHi     =  0x30,
-               .tTrnLo     =  0x34,
-               .tDueHi     =  0x38,
-               .tDueLo     =  0x3C,
-               .capacity   =  0x40,
-               .msgMax     =  0x44,
-               .ebmAdr     =  0x48,
-               .tsAdr      =  0x4C,
-               .tsCh       =  0x50,
-               .cfg_ENA             = 1<<0,
-               .cfg_FIFO            = 1<<1,    
-               .cfg_IRQ             = 1<<2,
-               .cfg_AUTOPOP         = 1<<3,
-               .cfg_AUTOFLUSH_TIME  = 1<<4,
-               .cfg_AUTOFLUSH_MSGS  = 1<<5,
-               .cfg_MSG_ARR_TS      = 1<<6,
-               .force_POP           = 1<<0,
-               .force_FLUSH         = 1<<1
-};
+#endif
